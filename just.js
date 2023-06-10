84ad6122b47e2e8a607be864540ebd249b8cfa66
enochdev2 <125772555+enochdev2@users.noreply.github.com>
2023-06-09 17:46:17 -0700

    Add files via upload

---
+++ just.js
@@ -0,0 +1,20 @@
+const forms = document.querySelector('.form');
+const name = document.querySelector('.name');
+const email = document.querySelector('.email');
+const phone = document.querySelector('.phone');
+
+
+
+
+forms.addEventListener("submit", function(e){
+   e.preventDefault();
+  const  nameEl = name.value;
+  const emailEl = email.value;
+  const phoneEl = phone.value;
+   //console.log('clicked
+   if(nameEl < 5){
+     nameEl.attri
+   }
+})
+/*forms.addEventListener('submit', function(e){
+});*/
